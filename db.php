7a7e806635cabd54bbba5e1224632c76b0fd0553
yogitabadiger <159226439+yogitabadiger@users.noreply.github.com>
2025-11-07 00:18:17 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,13 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = ""; // leave empty in WAMP
+$username = "root";      // default in WAMP
+$password = "";          // leave empty unless you set one
 $dbname = "student_registration";
 
+// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
